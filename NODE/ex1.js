877bbd8337d13c3899f59db7a1c3093b74697aca
bh-vana <108022255+bh-vana@users.noreply.github.com>
2023-02-26 20:16:36 +0530

    Add files via upload

---
+++ NODE/ex1.js
@@ -0,0 +1,28 @@
+﻿const fs = require('fs');
+fs.writeFile("text.txt","utf8",function(err,data){
+    if(err) throw err;
+    console.log(data);
+
+});
+//---- READ A FILE----
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
